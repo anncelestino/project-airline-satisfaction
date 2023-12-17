7885eeeae45473acdfbca56d6e24cf639d4cba53
anncelestino <you@example.com>
2023-12-16 21:51:52 -0800

    pushing work

--- code/app.py
+++ code/app.py
@@ -1,87 +1,204 @@
+import urllib.request
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+
+import requests
+import streamlit as st
+
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import ConfusionMatrixDisplay
 
-# Set page title and icon
-st.set_page_config(page_title = "Welcome to The Airline Passenger Satisfaction Dataset Explorer", page_icon = '✈︎')
+from pandas import json_normalize
+from streamlit_extras.add_vertical_space import add_vertical_space
+from streamlit_lottie import st_lottie
+from streamlit_player import st_player
 
-# Sidebar navigation
-page = st.sidebar.selectbox("Select a Page", ["Home 🏡", "Data Overview 📚", "EDA 🔍", "Modeling"])
-
+### Reading CSVs
 df = pd.read_csv('/Users/ann/Documents/coding-temple/05-week/project-airline-satisfaction/data/cleaned_train.csv')
 df_test = pd.read_csv('/Users/ann/Documents/coding-temple/05-week/project-airline-satisfaction/data/cleaned_test.csv')
 df1 = pd.read_csv('/Users/ann/Documents/coding-temple/05-week/project-airline-satisfaction/data/train.csv')
 df2 = pd.read_csv('/Users/ann/Documents/coding-temple/05-week/project-airline-satisfaction/data/test.csv')
 
+
+
+### Cleaning the data that had a hard time transfering to this app
 df.drop(columns = ['Unnamed: 0'], inplace = True)
 df_test.drop(columns = ['Unnamed: 0'], inplace = True)
 df1.drop(columns = ['Unnamed: 0'], inplace = True)
 df2.drop(columns = ['Unnamed: 0'], inplace = True)
 
-# Build a homepage
-if page == "Home 🏡":
-    st.title("🛩️ Airline Passenger Satisfaction Dataset Explorer App ✈︎")
-    st.subheader("✨Welcome to our Passenger Airline Satisfaction Dataset Explorer App✨")
-    st.write("> This app is designed to showcase different data visualizations of passenger flights based on The Airline Passenger Satisfaction Dataset!")
-    st.image("https://www.nerdwallet.com/assets/blog/wp-content/uploads/2021/04/GettyImages-172672886.jpg")
-    st.write("DISCLAIMER❗️: App is still a work in progress 🔧 Last Updated: Decemember 15, 2023")
 
-# Build a Data Overview page
-if page == "Data Overview 📚":
+
+# Set page title and icon
+st.set_page_config(page_title = "✈︎ Analyzing Passenger Satisfaction Based on Airline Statistics", page_icon = '✈︎', layout = 'wide')
+
+
+
+### Sidebar navigation
+page = st.sidebar.selectbox("Select a Terminal", ["1. Home 🏡", "2. Data Overview 📚", "3. EDA 🔍", "4. Modeling ⚙️"])
+
+
+### Adding music
+st.write("🎵 Here's some inflight cabin music while you browse our app 💺 Buckle up and enjoy!")
+
+audio_file1 = open('/Users/ann/Downloads/_The_Girl_from_Ipanema_Astrud_Gilberto_João_Gil.mp3', 'rb')
+audio_file2 = open('/Users/ann/Downloads/Elton_John_Rocket_Man_Official_Music_Video_.mp3', 'rb')
+audio_file3 = open('/Users/ann/Downloads/B_o_B_Airplanes_feat_Hayley_Williams_of_Paramo.mp3', 'rb')
+audio_file4 = open('/Users/ann/Downloads/Leaving_On_a_Jet_Plane_Greatest_Hits_Version_.mp3', 'rb')
+audio_bytes = audio_file1.read() + audio_file2.read() + audio_file3.read() + audio_file4.read()
+st.audio(audio_bytes, format='audio/mp3')
+
+st.write("---") 
+
+### Build a homepage
+if page == "1. Home 🏡":
+
+    col1_spacer, col1, col2_spacer, col2, col3_spacer = st.columns([0.1, 2, 0.2, 1, 0.1])
+
+    with col1:
+
+        st.title("✈︎ Analyze Airline Passenger Experiences")
+        st.subheader("Using ✨ The Passenger Airline Satisfaction Dataset Explorer App ✨")
+
+    with col2:
+
+        def load_lottieurl(url: str):
+            r = requests.get(url)
+            if r.status_code != 200:
+                return None
+            return r.json()    
+       
+        lottie_plane = load_lottieurl("https://lottie.host/b236d22b-9047-4f9a-8fdf-fb3bb30c451d/xtT4IRQYw8.json")        
+        st_lottie(lottie_plane, speed=1, height=250, key="initial")
+
+    st.write("---") 
+
+    col1_spacer, col1, col2, col3_spacer = st.columns([0.1, 1, 2, 0.1])
+    
+    with col1:
+
+        st.subheader("✈︎ A Streamlit web app by [Ann Celestino]('https://github.com/anncelestino/project-airline-satisfaction')")
+
+        # Load icon 2 
+        lottie_plane2 = load_lottieurl("https://lottie.host/875a432b-86ec-4e05-b079-78ddcb4f452a/wxJ80xYcZP.json")        
+        st_lottie(lottie_plane2, speed=1, height=150)
+
+        st.write(">Fly with us to see the different data visualizations of passenger flights based on 'The Airline Passenger Satisfaction Dataset'!")
+        
+    with col2:
+
+        st.image("https://www.travelandleisure.com/thmb/HbXDD_AhSlJXHm_iduNoUMPDxnU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-airport-gate-wait-layover-passenger-LAYOVERTIME0623--6c159c00aaf448de8baf97087490cad4.jpg")
+        
+
+
+
+
+### Build a Data Overview page
+if page == "2. Data Overview 📚":
+
     st.title("🔢 Data Overview")
-    st.subheader("About the Data")
-    st.write("> This dataset consists of various columns on airline passenger flight information ranging from delays to satisfaction rates.")
-    st.image("https://www.travelandleisure.com/thmb/HbXDD_AhSlJXHm_iduNoUMPDxnU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-airport-gate-wait-layover-passenger-LAYOVERTIME0623--6c159c00aaf448de8baf97087490cad4.jpg")
-    st.write("> The original dataset was obtained from Kaggle. The dataset can also be downloaded below!")
-    st.link_button("Kaggle Link", "https://www.kaggle.com/datasets/teejmahal20/airline-passenger-satisfaction/data", help = "Dataset Kaggle Page")
+
+    col1_spacer, col1, col2_spacer, col2, col3_spacer = st.columns([0.1, 2, 0.2, 2, 0.1])
     
-    # Download
-    @st.cache_data
-    def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
+    with col1:
+        st.subheader("About the Data")
+        st.write(">This dataset consists of various columns on airline passenger flight information ranging from delays to satisfaction rates. The original dataset was obtained from Kaggle which you can find [here]('https://www.kaggle.com/datasets/teejmahal20/airline-passenger-satisfaction/data'). The dataset can also be downloaded below!")
+        # Download
+        @st.cache_data
+        def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+        csv = convert_df(df1)
+        st.download_button(
+            label="Download dataset as CSV",
+            data=csv,
+            file_name='train.csv',
+            mime='text/csv')
+        
+    with col2:
+        st.image("https://www.nerdwallet.com/assets/blog/wp-content/uploads/2021/04/GettyImages-172672886.jpg")
 
-    csv = convert_df(df1)
-
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name='train.csv',
-        mime='text/csv')
-
+    st.write("---")
     
-    st.subheader("Quick Glance at the Data 🧐")
+    
+    st.subheader("Quick Glance at the Airline Dataset 🧐")
+
+    def load_lottieurl(url: str):
+            r = requests.get(url)
+            if r.status_code != 200:
+                return None
+            return r.json()  
+    lottie_data_overview = load_lottieurl("https://lottie.host/ff8b9c06-101e-41e2-b2e2-5c0d0c0202d7/8ySYj8rXJN.json")        
+    st_lottie(lottie_data_overview, speed=1, height=100, key="initial")
+
+    container = st.container(border=True)
+    container.write("Make your selection(s):")
+    st.write("Here are your choices ↴")
 
     # Display dataset
-    if st.checkbox("DataFrame"):
+    if container.checkbox("Data Frame"):
         st.dataframe(df1)
 
     # Column List
-    if st.checkbox("Column List"):
+    if container.checkbox("Column List"):
         st.code(f"Columns: {df1.columns.tolist()}")
 
         if st.toggle('Further breakdown of columns'):
             num_cols = df1.select_dtypes(include = 'number').columns.tolist()
             obj_cols = df1.select_dtypes(include = 'object').columns.tolist()
             st.code(f"Numerical Columns: {num_cols} \nObject Columns: {obj_cols}")
-    if st.checkbox("Shape"):
+    
+    # Shape
+    if container.checkbox("Shape"):
         st.write(f"There are {df.shape[0]} rows and {df.shape[1]} columns.")
 
+
+
+
+
+
+
 # Build EDA page
-if page == "EDA 🔍":
-    st.title("🔍 EDA")
+if page == "3. EDA 🔍":
+
+    col1_spacer, col1, col2, col3_spacer = st.columns([0.1, 2, 5, 0.1])
+
+    with col1:
+        st.title("🔍 EDA")
+
+    with col2:
+        def load_lottieurl(url: str):
+            r = requests.get(url)
+            if r.status_code != 200:
+                return None
+            return r.json()  
+        lottie_eda = load_lottieurl("https://lottie.host/1d1bb74c-d94f-40ef-81ef-34e183a92387/aPEu6GgfEQ.json")        
+        st_lottie(lottie_eda, speed=1, height=200, key="initial")
+    
     num_cols = df.select_dtypes(include = 'number').columns.tolist()
     obj_cols = df.select_dtypes(include = 'object').columns.tolist()
+
+    container = st.container(border=True)
+
+    
 
     eda_type = st.multiselect("What type of EDA are you interested in exploring?", 
                               ['Histograms', 'Box Plots', 'Scatterplots', 'Count Plots'])
     
+
+    # COUNTPLOTS
+
+
+
+
+
+
     # HISTOGRAMS
     if "Histograms" in eda_type:
         st.subheader("Histograms - Visualizing Numerical Distributions")
@@ -141,7 +258,7 @@ if page == "EDA 🔍":
                 st.plotly_chart(px.scatter(df, x = selected_col_x, y = selected_col_y, title = chart_title))
 
 # Build a Markdown Page
-if page == "Modeling":
+if page == "4. Modeling ⚙️":
     st.title(":gear: Modeling")
     st.markdown("**On this page, you can see how well different machine learning models** make predictions on the satisfaction rates!")
 
@@ -172,6 +289,8 @@ if page == "Modeling":
         st.pyplot()
 
 
+st.write("---") 
+st.write("🔧 Last Updated: Decemember 15, 2023") 
 
 # Predictions Page
 # if page == "Make Predictions!":
